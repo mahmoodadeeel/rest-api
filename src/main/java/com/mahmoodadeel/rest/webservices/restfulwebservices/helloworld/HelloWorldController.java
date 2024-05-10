891353b67e27ef3d6248247a87ffff01339c96bf
“Mahmood <mahmoodadeel84@gmail.com>
2024-05-10 20:13:48 +0500

    adding path variable.

--- src/main/java/com/mahmoodadeel/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/mahmoodadeel/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.mahmoodadeel.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +18,8 @@ public class HelloWorldController {
 		return new HelloWorldBean("Hello World Bean");
 	}
 
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldBeanPathvariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World %s", name));
+	}
 }
