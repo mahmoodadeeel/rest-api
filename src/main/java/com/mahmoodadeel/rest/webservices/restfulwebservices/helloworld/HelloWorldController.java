154c8f9f51e841bf169397aceea68ecc819f52e8
“Mahmood <mahmoodadeel84@gmail.com>
2024-05-10 20:03:16 +0500

    adding a bean.

--- src/main/java/com/mahmoodadeel/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/mahmoodadeel/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	
 	@GetMapping(path = "/hello-world")
-	public String helloworld() {
+	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World Bean");
 	}
 
 }
