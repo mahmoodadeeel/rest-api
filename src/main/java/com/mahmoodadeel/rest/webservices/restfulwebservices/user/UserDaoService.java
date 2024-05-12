097815053c0e61ddc5e6bfe554bde692878d47a6
“Mahmood <mahmoodadeel84@gmail.com>
2024-05-12 10:48:24 +0500

    Error Handlid: User not found

--- src/main/java/com/mahmoodadeel/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/mahmoodadeel/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -25,7 +25,8 @@ public class UserDaoService {
 	
 	public User findOne(int id) {
 		Predicate<? super User> predicate =user -> Integer.valueOf(user.getId()).equals(id);
-		return users.stream().filter(predicate).findFirst().get();
+		//return users.stream().filter(predicate).findFirst().get();
+		return users.stream().filter(predicate).findFirst().orElse(null);
 	}
 	
 	public User save(User user) {
--- src/main/java/com/mahmoodadeel/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/mahmoodadeel/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,6 +1,7 @@
 package com.mahmoodadeel.rest.webservices.restfulwebservices.user;
 
 import java.net.URI;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -27,7 +28,10 @@ public class UserResource {
 	
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id){
-		return service.findOne(id);
+		User user = service.findOne(id);
+		if(user == null) 
+			throw new UserNotFoundException(":id"+id);
+		return user;
 	}
 	
 	@PostMapping("/users")
