a479887698f9757c8e613488be6ce4d5a2c2ac6b
“Mahmood <mahmoodadeel84@gmail.com>
2024-05-12 14:52:40 +0500

    adding custom error messages class.

---
+++ src/main/java/com/mahmoodadeel/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.mahmoodadeel.rest.webservices.restfulwebservices.exception;
+
+import java.time.LocalDate;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(),ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
